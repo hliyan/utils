28b26cd315b62ad4d5cd11e9cc5eff155d7fb7ba
hliyan <hasithaliyan@gmail.com>
2017-11-13 21:49:46 +0530

    index.js; test changes

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import {is, check} from './is';
+import Client from './client';
+
+export is;
+export check;
+export Client;
--- test/is.test.js
+++ test/is.test.js
@@ -8,28 +8,28 @@ const isObject = {type: 'object'};
 const isFunction = {type: 'function'};
 const isCorrectLength = {length: [5,10]};
 
-test('validate type: ignore typos in the type rule', () => {
+test('is type: ignore typos in the type rule', () => {
   expect(is(1000, {type: ''})).toBe(null);
 });
 
-test('validate type: string', () => {
+test('is type: string', () => {
   expect(is('foo', isString)).toBe(null);
   expect(is(1000, isString)).toEqual(isString);
 });
 
-test('validate type: number', () => {
+test('is type: number', () => {
   expect(is(1000, isNumber)).toBe(null);
   expect(is(null, isNumber)).toEqual(isNumber);
 });
 
-test('validate type: integer', () => {
+test('is type: integer', () => {
   expect(is(1000, isInteger)).toBe(null);
   expect(is(1.5, isInteger)).toEqual(isInteger);
 });
 
 const isRange = { type: 'integer', range: [1, 5] };
 
-test('validate type + range', () => {
+test('is type + range', () => {
   expect(is(2, isRange)).toBe(null);
   expect(is(6, isRange)).toEqual({range: [1, 5]});
   expect(is(-1, isRange)).toEqual({range: [1, 5]});
@@ -37,30 +37,29 @@ test('validate type + range', () => {
   expect(is(9.5, isRange)).toEqual({type: 'integer', range: [1, 5]});
 });
 
-test('validate type: boolean', () => {
+test('is type: boolean', () => {
   expect(is(false, isBoolean)).toBe(null);
   expect(is(0, isBoolean)).toEqual(isBoolean);
 });
 
 
-test('validate type: object', () => {
+test('is type: object', () => {
   expect(is({}, isObject)).toBe(null);
   expect(is(0, isObject)).toEqual(isObject);
 });
 
-test('validate type: function', () => {
+test('is type: function', () => {
   expect(is(() => {}, isFunction)).toBe(null);
   expect(is({}, isFunction)).toEqual(isFunction);
 });
 
-test('validate length', () => {
+test('is length', () => {
   expect(is('foobar', isCorrectLength)).toEqual(null);
   expect(is('laskdfjlaksdjfalskd', isCorrectLength)).toEqual(isCorrectLength);
   expect(is('foo', isCorrectLength)).toEqual(isCorrectLength);
 });
 
-// guard test:
-test('guard length', () => {
+test('check length', () => {
   expect(() => {
     check({foo: 'bar'}, {type: 'string', length: [5,10]});
   }).toThrow('Value of [foo] does not match following criteria: ' + 
@@ -72,7 +71,7 @@ test('guard length', () => {
 
 const isRegex = {regex: /[a-z]+/};
 
-test('validate regex', () => {
+test('is regex', () => {
   expect(is('foobar', isRegex)).toEqual(null);
   expect(is('ADDDDDD', isRegex)).toEqual(isRegex);
 });
