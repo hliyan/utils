0c74ba1e4a58e465b19a23104dc7315324fa3f6e
hliyan <hasithaliyan@gmail.com>
2017-11-13 21:52:02 +0530

    moved index.js to root

---
+++ index.js
@@ -0,0 +1,6 @@
+import {is, check} from './src/is';
+import Client from './src/client';
+
+export is;
+export check;
+export Client;
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import {is, check} from './is';
-import Client from './client';
-
-export is;
-export check;
-export Client;
